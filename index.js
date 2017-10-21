777bfdee8281f4b850e0d9c3c2334e5965f2da1f
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-10-21 15:56:51 -0600

    html

---
+++ index.js
@@ -0,0 +1,70 @@
+$(function () {
+//create a click event to solve puzzle
+//create word array to diplay words
+//be able to click on letter
+//
+
+
+
+
+class Game() {
+	constructor() {
+		this.abcArray = [];
+		this.clueArray = clues;
+		this.clue = clues[math.random()]
+
+	}
+}
+
+	guessLetter() {
+
+	}
+
+
+
+
+
+
+//create a for loop to populate the letters on the page
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
