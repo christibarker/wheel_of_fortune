2087b13ab6c2f210e59dc7bc05dbd79cdf14b641
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-10-21 16:18:44 -0600

    styling2

--- index.js
+++ index.js
@@ -1,34 +1,73 @@
-$(function () {
-//create a click event to solve puzzle
-//create word array to diplay words
-//be able to click on letter
-//
+const phraseArray = ['happy'];
+
+
+class Game {
+
+  constructor(phraseArr){
+
+    this.phraseArr = phraseArr;
+    this.phrase = this.phraseArr[0].split('');
+    this.correctLetters = [];
+    this.abcArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    this.phraseLetters =  this.phrase.filter(function(elem, index, self) 
+    {
+      return index == self.indexOf(elem); 
+    }) // Eliminates duplicate letters for clue checker
+
+  } // End of constructor
+
+  checkIfWon(){
+    if (this.correctLetters.length == this.phraseLetters.length) {
+      alert('You win!');
+    }
+    else {
+      this.guessLetter();
+    }
+  } // End of checkIfWon method
+
+  displayClue(){
+
+  }
+
+  guessLetter(){
+    let letter = prompt('Guess a letter!');
+
+
+    if(this.phrase.includes(letter)){
+      if(this.correctLetters.includes(letter)){
+        alert("You've already guessed that letter!");
+        this.guessLetter();
+      }
+      else{
+      this.correctLetters.push(letter);
+      }
+    }
+
+    this.checkIfWon();
+
+  }
 
 
 
 
-class Game() {
-	constructor() {
-		this.abcArray = [];
-		this.clueArray = clues;
-		this.clue = clues[math.random()]
-
-	}
 }
 
-	guessLetter() {
-
-	}
 
 
 
 
 
 
-//create a for loop to populate the letters on the page
+
+$(function() { // Document ready function
+  
+  let wof = new Game(phraseArray);
+
+  wof.displayClue();
+  wof.checkIfWon();
 
 
-
+}); // End of Document Ready Function
 
 
 
