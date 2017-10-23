77fe177c917713ccaa6e2c5b26b9fbeaf78b6b09
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-10-23 11:14:36 -0600

    java

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
 
 $(function(){
 
-// var addMoney = (letter * $300)
-// var subMoney = (letter - $200)
+	var newGame = new Game();
+
+$('#player').on('click', function(){
+	startGame();
+})
+
 
 //when letter is clicked the letter shows on screen or guess is counted wrong
 
-// $('.letters').on('click', function(){
-// 	var letter = $(.letters);
-	//if (lettter correct) {
-		// then diplay on screen & add money to their total
-		// $('#innerText').html('');
-	// } else {
-			// wrong guess
-			// minus that money from them
-	// }
-
-	// 	});
-
-	// });
+$('.letters').on('click', function(){
+	letterGuessed();
+	checkIfWon();
+	});
 
 //guess whole word
- // $(#guess).on('click', function(){
- // 	if (answer === to guess){
- // 		$('#win_lose').html('you won' + $500 bonus);
- // 	} else {
- // 		minus $200
- 			// alert('incorrect, you lose $200')
- // 	}
- // })
+ $('#solve').on('click', function(){
+ 	prompt('enter guess');
+ 	if (answer === checkIfWon()){
+ 		checkIfWon();
+ 	} else {
+ 		subMoney();
+ 		alert('incorrect, you lose $200')
+ 	}
+ })
 
 
 //if they win fade in 
@@ -40,13 +36,62 @@ $(function(){
 class Game {
 	constructor(){
 		this.abcArray = [];
+		this.phrases = ['Happy', 'Disney', 'monkey'];
+		this.correctLetter = [];
 	}
 
 //this pushes letters to the abc array when clicked
 	abcArray(){
-		this.abcArray = $('letters')
+		this.abcArray = $('letters');
 	}
 
+	startGame(phrases){
+		this.background = $('#innerText_top').css("background", "white");
+		wordSelector();
+	}
+//randomly selects words and changes background in innerText div
+	wordSelector(phrases){
+		this.phrases = phrases[(int) (Math.random() * phrases.length)];
+		
+// 		private static String[] names = { "Terminator", "Slicer","Ninja", "cow", "Robot", "littlegirl" };
+// name = names[(int) (Math.random() * names.length)];
+// System.out.println(name);
+	}
+
+	letterGuessed(){
+		
+		this.correctLetter.push([]);
+		// var correct = $('.letters');
+		if('.letters' === this.phrases){
+			$('#innerText_top').html( 'phrases' + addMoney());
+		} else {
+			alert('incorrect');
+			subMoney();
+		}
+
+	}
+
+	solvePuzzle(){
+		this.wordSelector;
+
+	}
+
+	checkIfWon(){
+		$('#win_lose').html('you won' + '$500 bonus' + addMoney());
+	}
+
+	//add money to their total
+	addMoney(){
+		var addMoney = ('letter' * parseInt(300));
+		this.phrases + addMoney;
+		// letter fades
+	}
+
+//subtract money from their total
+	subMoney(){
+		var subMoney = ('letter' - parseInt(200));
+		this.phrases - subMoney;	
+		// minus that money from them
 
 }
 
