5ea855de33498697ed0300de4859bafa34f20119
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-10-24 12:35:23 -0600

    letterguess

--- index.js
+++ index.js
@@ -12,40 +12,34 @@ $(function(){
 	var newGame = new Game();
 
 	//click on and new word is generated, background to display word changes color
-	$('#player').on('click', function(){
-		// $('.col').css({"background": "white"});
-		// alert('it works');
-		newGame.startGame();
-	})
+		$('#player').on('click', function(){
+			newGame.startGame();
+		})
 
-	$('#player_t').on('click', function(){
-		// $('.col').css({"background": "white"});
-		// alert('it works');
-		newGame.startGame();
-	})
+		$('#player_t').on('click', function(){
+			newGame.startGame();
+		})
 
 
 	//when letter is clicked the letter shows on screen or guess is counted wrong
-	$('.letters').on('click', function(){
-		// var letter = $(this).data('letter');
-		// $('#A').fadeOut('slow'); use letter id
-			 //loop through id's
-		// alert('working');
-		// newGame.letterGuessed();
-		// newGame.checkIfWon();
-		});
+		$('.letters').on('click', function(){
+			var letter = $(this).data('letter');
+			$('letter').fadeOut('slow');
+			console.log(letter);
+			newGame.letterGuessed(letter);
+			// newGame.checkIfWon();
+			});
 
-	// //guess whole word/phrase
-	 $('#solve').on('click', function(){
-	 	prompt('enter guess');
-	 	if (guess === solvePuzzle()){
-	 		checkIfWon();
-	 	} else {
-	 		subMoney();
-	 		alert('incorrect, you lose $200')
-	 	}
-	 })
-
+		// //guess whole word/phrase
+		 $('#solve').on('click', function(){
+		 	prompt('enter guess');
+		 	if (guess === solvePuzzle()){
+		 		checkIfWon();
+		 	} else {
+		 		subMoney();
+		 		alert('incorrect, you lose $200')
+		 	}
+		 })
 
 });
 //buy a vowel should pop up a modal with list of vowels to buy
@@ -55,60 +49,70 @@ $(function(){
 class Game {
 	constructor(){
 		this.phrases = ['Happy', 'Disney', 'monkey'];
-    	this.phrase = this.phrases[0].split('');//splits words into letters
-		this.correctLetter = [];
-		this.letter = $(this).data('letter');//get data name;
-		this.letterBox = $(this).data('box');//get data name
+    this.phrase = this.phrases[0].split('');//splits words into letters
+		this.correctLetter = [''];
+		this.boardWidth = 8;
 	}
 
 //this pushes letters to the correctLetter array when clicked
 	correctLetter(){
-		this.letterGuessed = this.correctLetter.push([]);
+		this.phrase
+		this.correctLetter = this.correctLetter.push([]) === this.letterGuessed;
 	}
 
 //randomly selects words and changes background in innerText div
 	startGame(){
-		console.log('startGame!')
-		// var wordSelector = this.phrases
-		var background = $('.col').css({"background": "white"});
-		var offset = Math.floor((8 - this.phrase.length) / 2);
+		// console.log('startGame!')
+		var offset = Math.floor((this.boardWidth - this.phrase.length) / 2);
 
 		this.phrases = this.phrases[Math.floor(Math.random() * this.phrases.length)]
-		// [(int) (Math.random() * this.phrases.length)];
-	// [Math.floor(Math.random() * this.phrases.length)] opt 2	
 
 		//this need to display white boxes for the amount of letters in word
 		for (var i = offset; i < (offset + this.phrase.length); i++) {
-			console.log("box at " + i)
-			//function(index, value){
-			//$('#t_' + i);
+			// console.log('#t_' + i)
+			$('#t_' + i).css({"background": "white"});
 		};
-
-		// {
-		// 	$(this.letterBox).html(background + offset);
-		// };
-
-	// 	var newHTML = [];
-	// $.each(pokemonArray, function(index, value) {
-	//     newHTML.push('<div data-name="' + value + '" class="child-pokemon">' + value + '</div>');
-	// });
-	// $('.pokemon').html(newHTML);
 	}
 
-//guess letter if correct one thing else alert incorrect
-	// letterGuessed(){
-		// var letter = $(this).data('letter');
+
+	letterGuessed(letter){
+		// // guess letter if correct/ else alert incorrect
+
+		for (var i = letter; i < letter + (letter.length); i++);
+			console.log(letter)
+		//adds letter to board	
+		$('#t_1').text(letter);
+		// var inWord = false;
+		
+		// if(this.phrase includes($(this).data('letter'))){
+		// 	if($(this).data('letter') === letter(inWord = true);
+
+		// 	$('#t_' + i).html(this.correctLetter);
+
+		// 	// addMoney();
+		// } else {
+		// 	alert('incorrect');
+		// 	// subMoney();
+		// }
+
+
+		//var word = false;
+		// get letter
+		// loop through data-letter
+		// if not letter blank
+		// 	if data-letter word=true;
+		// 
+
+
+
 		// if(this.phrase includes(this.letter)){
-		// 	$(this.letterBox).html(this.correctLetter);
+		// 	$('#t_' + i).html(this.correctLetter);
 		// 	addMoney();
 		// } else {
 		// 	alert('incorrect');
 		// 	subMoney();
 		// }
-		//  $('#').fadeOut(3000);
-		 //loop through id's
-		
-	// }
+	};
 
 	// solvePuzzle(){
 		
@@ -153,9 +157,8 @@ class Game {
 	// 	$('.money').html(totalMoney + '$');
 		
 	// 	return totalMoney;
-	// };
+	};
 
 
 
 
-};
